f337c92f358daa4d8494f71a734b74fc17c1a714
Lucas <lucas.johannsen@haw-kiel.de>
2026-02-04 13:31:57 +0100

    Revert "Feature/plugin release packaging (#9)" (#10)
    This reverts commit 4fda1780cbc179600ceadd3933b2ddd43776af63.

--- test/test_resources.py
+++ test/test_resources.py
@@ -18,7 +18,7 @@ from qgis.PyQt.QtGui import QIcon
 
 
 
-class OFEFilterDialogTest(unittest.TestCase):
+class OFRFilterDialogTest(unittest.TestCase):
     """Test rerources work."""
 
     def setUp(self):
@@ -31,12 +31,12 @@ class OFEFilterDialogTest(unittest.TestCase):
 
     def test_icon_png(self):
         """Test we can click OK."""
-        path = ':/plugins/ofe_filter/icon.png'
+        path = ':/plugins/OFRFilter/icon.png'
         icon = QIcon(path)
         self.assertFalse(icon.isNull())
 
 if __name__ == "__main__":
-    suite = unittest.makeSuite(OFEFilterResourcesTest)
+    suite = unittest.makeSuite(OFRFilterResourcesTest)
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
 
